0948744af06332ea731df46b0c852424f34439ac
vitvolny26-art <vitvolny26@gmail.com>
2026-08-27 06:47:59 +0200

    GROOMING011: remove Vercel function-limit blocker
    Commit: 20e4d7c
    
    Consolidate Instagram/Messenger test-invitation routing to reduce Vercel serverless function count from 13 to 12.

--- api/instagram/test-invitation.ts
+++
@@ -1,4 +0,0 @@
-import { handleInstagramTestInvitation } from "../_shared/instagram-test-invitation.js";
-import { createVercelHandler } from "../_shared/vercel-handler.js";
-
-export default createVercelHandler(handleInstagramTestInvitation);
