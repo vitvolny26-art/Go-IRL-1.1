1d7872fe25fc2c6626b99c405e78d2894750d13a
vitvolny26-art <vitvolny26@gmail.com>
2026-09-15 13:55:32 +0200

    Cinema: route approval to Mini App surface

--- src/app-entry.ts
+++ src/app-entry.ts
@@ -2,6 +2,8 @@ const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
 
 if (normalizedPath === "/city-posters") {
   void import("./city-posters/entry");
+} else if (normalizedPath === "/cinema/approval") {
+  void import("./cinema-approval/entry");
 } else {
   void import("./main");
 }
